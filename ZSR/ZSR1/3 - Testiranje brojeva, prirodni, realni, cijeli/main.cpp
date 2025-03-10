2799944e902390337abd6a6cdcc3e32a0a86795b
rehicaa <rehapremium@gmail.com>
2025-03-10 18:31:12 +0100

    Uradjen zadatak 3 ZSR1

--- ZSR/ZSR1/3 - Testiranje brojeva, prirodni, realni, cijeli/main.cpp
+++ ZSR/ZSR1/3 - Testiranje brojeva, prirodni, realni, cijeli/main.cpp
@@ -5,24 +5,21 @@ int main() {
   std::cout << "Unesite broj: " << std::endl;
   std::cin >> broj;
 
-  if(std::cin.fail()){
+  if(!std::cin){
     std::cout << "Uneseni podatak nije broj." << std::endl;
     return 1;
   }
-
-  if (broj > 0) {
-    std::cout << "Uneseni podatak je prirodan broj." << std::endl;
-  }
-  else if (broj = static_cast<int>(broj) && broj < 0) {
+  if (broj == static_cast<int>(broj) && broj <= 0) {
     std::cout << "Uneseni podatak je cijeli broj, ali nije prirodan." << std::endl;
   }
 
-  /* if (broj != static_cast<int>(broj)) {
-    std::cout << "Uneseni broj je realnog tipa." << std::endl;
-  } else {
-      std::cout<< "Uneseni broj je cijeli broj." << std::endl;
-    }
-*/
+  else if (static_cast<int>(broj) != broj) {
+    std::cout << "Uneseni broj je realan, ali nije cijeli." << std::endl;
+  }
+
+  else if (broj > 0) {
+    std::cout << "Uneseni podatak je prirodan broj." << std::endl;
+  }
 
   return 0;
-  }
+}
