340e05d3f51a0e5b4a655a4b76ddbf51de0979ab
rehicaa <rehapremium@gmail.com>
2025-03-17 01:20:20 +0100

    Zavrsen zadatak 10 ZSR1

--- ZSR/ZSR1/10 - Rastava broja na proste faktore bez ponavljanja/main.cpp
+++ ZSR/ZSR1/10 - Rastava broja na proste faktore bez ponavljanja/main.cpp
@@ -1,3 +1,28 @@
-//
-// Created by rehaaznii on 3/11/25.
-//
+#include <iostream>
+using std::endl;
+using std::cin;
+using std::cout;
+
+void rastaviNaProsteFaktore(int broj) {
+    int faktor = 2;
+
+    while (broj > 1) {
+        while (broj % faktor == 0) {
+            cout << faktor << " ";
+            broj = broj / faktor;
+        }
+        faktor= faktor + 1;
+    }
+}
+
+int main(){
+    int broj;
+    cout << "Unesi broj koji zelis rastaviti na proste faktore: ";
+    cin >> broj;
+
+    if (broj < 2) {
+        cout << "Unesite broj veći od 1." << endl;
+
+    } else rastaviNaProsteFaktore(broj);
+
+}
