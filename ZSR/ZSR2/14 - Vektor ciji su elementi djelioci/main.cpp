f8f71ad30a5f48ed7491cd6748ff3ac8aa637504
rehicaa <rehapremium@gmail.com>
2025-03-17 20:08:37 +0100

    Uradjen zadatak 14 ZSR2

---
+++ ZSR/ZSR2/14 - Vektor ciji su elementi djelioci/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using std::cout, std::cin, std::endl, std::vector, std::log;
+
+vector<int>vratiDjelioceBroja(int n) {
+    vector<int>x;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            x.push_back(i);
+        }
+    }
+    return x;
+}
+
+int main(){
+    cout << "Unesite prirodni broj: ";
+    int n;
+    cin >> n;
+
+    auto x = vratiDjelioceBroja(n);
+    for (int i = 0; i < x.size(); i++) {
+        cout << x[i] << " ";
+    }
+
+  }
