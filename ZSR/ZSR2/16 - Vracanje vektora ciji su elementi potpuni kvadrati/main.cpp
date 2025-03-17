39c85b00a671d776a07e5e4ed8e71e5224b18d96
rehicaa <rehapremium@gmail.com>
2025-03-17 21:26:26 +0100

    Uradjen zadatak 16 ZSR2

---
+++ ZSR/ZSR2/16 - Vracanje vektora ciji su elementi potpuni kvadrati/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using std::cout, std::cin, std::endl, std::vector, std::log;
+
+vector<int>vratiPotpuneKvadrate(int n) {
+    vector<int>x;
+    for (int i = 1; i < n; i++) {
+        if (std::sqrt(i) == static_cast<int>(std::sqrt(i))) {
+            x.push_back(i);
+        }
+    }
+    return x;
+}
+
+int main(){
+    cout << "Unesite prirodan broj: ";
+    int n;
+    cin >> n;
+
+    auto x = vratiPotpuneKvadrate(n);
+    for(int i = 0; i < x.size(); i++) {
+        cout << x[i] << " ";
+    }
+}
